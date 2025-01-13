94d6f96b82042c0ff2176654d8059670eb211a2f
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-13 13:48:41 -0300

    .

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 num_workers=20
 epochs=5000
+alpha=.15
+beta=.6
 # num_workers=2
 # epochs=20
 
@@ -9,8 +11,8 @@ epochs=5000
 for size in 0.1 0.25 0.5 0.75;
 # for size in 0.1;
 do
-    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha .2 --beta .75
-    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha .2 --beta .75
+    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha alpha --beta beta
+    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha alpha --beta beta
     python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest
     python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random
 done

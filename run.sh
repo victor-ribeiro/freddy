781865fe5d79f22eaefbebc70bd49bc129b7c460
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-14 15:00:19 -0300

    .

--- run.sh
+++ run.sh
@@ -1,20 +1,23 @@
 #/bin/bash
 
 num_workers=20
-epochs=5000
+epochs=50
 alpha=.15
 beta=.6
 # num_workers=2
 # epochs=20
 
 
-for size in 0.1 0.25 0.5 0.75;
-# for size in 0.1;
+for e in 0 50 100 150 200 250 300 350 400 450 500 550;
 do
-    # python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta &
-    # python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha $alpha --beta $beta &
-    python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest &
-    # python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random &
+    for size in 0.1 0.25 0.5 0.75;
+    # for size in 0.1;
+    do
+        python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta
+        python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha $alpha --beta $beta
+        python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --train_frac $size --selection_method crest
+        python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --train_frac $size --selection_method random
+    done
 done
 
-# python crest_train.py --num_workers $num_workers --selection_method none &
+python crest_train.py --num_workers $num_workers --selection_method none

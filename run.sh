c2dc723e2240c81bc9efa982bb26ee409a6bea98
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-17 11:33:17 -0300

    .

--- run.sh
+++ run.sh
@@ -28,6 +28,6 @@ size=.1
 
 for alpha in .25 .5 .75 1 2 3 5 10 20;
 do
-    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method --resume_from_epoch $e grad_freddy --alpha $alpha --beta $beta &
-    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method --resume_from_epoch $e freddy --alpha $alpha  --beta $beta &
+    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --resume_from_epoch $e  --alpha $alpha --beta $beta &
+    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --resume_from_epoch $e  --alpha $alpha  --beta $beta &
 done

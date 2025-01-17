a46d134e1e85ab67b0d1dcb1ba8d23afa17b107a
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-17 16:31:22 -0300

    .

--- run.sh
+++ run.sh
@@ -2,22 +2,21 @@
 
 num_workers=12
 epochs=40
-beta=1
+beta=.1
 alpha=1
-e=20
 size=.1
 # for size in 0.1 0.25 0.5 0.75;
 # do
-#     python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --freddy_similarity 'codist' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta
-#     python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --freddy_similarity 'codist' --train_frac $size --selection_method freddy --alpha $alpha --beta $beta
-#     python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --train_frac $size --selection_method crest
-#     python crest_train.py --num_workers $num_workers --epochs $epochs --resume_from_epoch $e --train_frac $size --selection_method random
+#     python crest_train.py --num_workers $num_workers --epochs $epochs  --freddy_similarity 'codist' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta
+#     python crest_train.py --num_workers $num_workers --epochs $epochs  --freddy_similarity 'codist' --train_frac $size --selection_method freddy --alpha $alpha --beta $beta
+#     python crest_train.py --num_workers $num_workers --epochs $epochs  --train_frac $size --selection_method crest
+#     python crest_train.py --num_workers $num_workers --epochs $epochs  --train_frac $size --selection_method random
 # done
 
 # CONTROLE
-# python crest_train.py --num_workers $num_workers --selection_method none --epochs $epochs --resume_from_epoch $e &
-# python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest  --resume_from_epoch $e &
-# python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random --resume_from_epoch $e &
+# python crest_train.py --num_workers $num_workers --selection_method none --epochs $epochs  &
+# python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest   &
+# python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random  &
 
 
 # BETA = 0
@@ -29,6 +28,6 @@ size=.1
 
 # for alpha in .25 .5 .75 1 2 3 5 10 20;
 # do
-python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --resume_from_epoch $e  --alpha $alpha --beta $beta &
-python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --resume_from_epoch $e  --alpha $alpha  --beta $beta &
+python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta &
+python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha $alpha  --beta $beta &
 # done

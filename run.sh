211bf548d07a7632970f42ec9b782283cadbd9f3
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-17 11:30:12 -0300

    .

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 num_workers=4
 epochs=20
-beta=0.5
+beta=0.
 e=0
 size=.1
 # for size in 0.1 0.25 0.5 0.75;
@@ -14,10 +14,9 @@ size=.1
 # done
 
 # CONTROLE
-python crest_train.py --num_workers $num_workers --selection_method none --epochs $epochs &
-python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest  &
-python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random &
-python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method craig &
+# python crest_train.py --num_workers $num_workers --selection_method none --epochs $epochs --resume_from_epoch $e &
+# python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest  --resume_from_epoch $e &
+# python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random --resume_from_epoch $e &
 
 
 # BETA = 0
@@ -29,6 +28,6 @@ python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $
 
 for alpha in .25 .5 .75 1 2 3 5 10 20;
 do
-    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta &
-    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha $alpha  --beta $beta &
+    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method --resume_from_epoch $e grad_freddy --alpha $alpha --beta $beta &
+    python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method --resume_from_epoch $e freddy --alpha $alpha  --beta $beta &
 done

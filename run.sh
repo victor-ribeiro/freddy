ea40a88320c0bd96b0d65b3ce0ff089afe19335b
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-17 08:57:29 -0300

    .

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-num_workers=20
-epochs=200
-beta=0.1
+num_workers=4
+epochs=20
+beta=0.5
 e=0
 size=.1
 # for size in 0.1 0.25 0.5 0.75;
@@ -17,6 +17,7 @@ size=.1
 python crest_train.py --num_workers $num_workers --selection_method none  &
 python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method crest  &
 python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method random &
+python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $size --selection_method craig &
 
 
 # BETA = 0
@@ -25,7 +26,8 @@ python crest_train.py --num_workers $num_workers --epochs $epochs --train_frac $
 # BETA = 0.1
 # feitos (alpha):
 
-for alpha in  1 1.25 1.5;
+
+for alpha in .25 .5 .75 1 2 3 5 10 20;
 do
     python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method grad_freddy --alpha $alpha --beta $beta &
     python crest_train.py --num_workers $num_workers --epochs $epochs --freddy_similarity 'similarity' --train_frac $size --selection_method freddy --alpha $alpha  --beta $beta &

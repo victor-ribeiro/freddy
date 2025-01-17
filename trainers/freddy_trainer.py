8c8aeae6bf864e1a2bb8579f2e0bb6ac375411c2
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-01-17 15:00:30 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -294,7 +294,7 @@ class FreddyTrainer(SubsetTrainer):
             )
         self._val_epoch(epoch)
         rel_err = abs(prev_loss - self.get_val_loss())
-        if epoch == 0 or abs(rel_err) < 10e-4:
+        if abs(rel_err) < 10e-4:
             self._select_subset(epoch, len(self.train_loader) * epoch)
         if self.args.cache_dataset and self.args.clean_cache_iteration:
             self.train_dataset.clean()

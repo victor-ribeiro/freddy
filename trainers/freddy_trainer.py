218d22cbe15c094c98bf78608f2d1df7ed6e47a3
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-05 10:41:21 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -349,7 +349,7 @@ class FreddyTrainer(SubsetTrainer):
 
         # self.subset_weights[data_idx] *= importance.reshape(-1, 1)
 
-        # self.importance_score[data_idx] += importance
+        self.importance_score[data_idx] += importance
         # self.importance_score[data_idx] = importance
         # self.importance_score[data_idx] -= importance
 

22b3cc0dd4d774c98a8476c4b12eca04a87956bf
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-05 16:25:24 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -323,12 +323,11 @@ class FreddyTrainer(SubsetTrainer):
         grad2 = [*self.model.to(self.args.device).modules()]
         grad2 = grad2.pop()
         grad2 = grad2.weight.grad.data.norm(2).item()
-        error = abs(grad2 - grad1) / self.importance_score[self.subset].mean()
+        error = abs(grad2 - grad1 / self.importance_score[self.subset].mean())
         print(f"relative error [{error:.4f}]")
         self.cur_error = error
         # if error / self.cur_error > error > 10e-2:
-        # if error < 10e-2:
-        if self.cur_error > error:
+        if error < 10e-2:
             self._select_subset(epoch, len(self.train_loader) * epoch)
 
     def _forward_and_backward(self, data, target, data_idx):

d5265a16e4b362a801b752c77023bb595a42f76a
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-05 11:41:14 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -265,7 +265,7 @@ class FreddyTrainer(SubsetTrainer):
         self.train_checkpoint["importance"] = self.importance_score
         self.train_checkpoint["epoch_selection"] = self.epoch_selection
         # score = np.concat(([0], np.diff(score))) / score
-        self.subset_weights *= self.importance_score[sset]
+        self.subset_weights *= self.importance_score[sset].reshape(-1, 1)
         # self.subset_weights = np.ones(self.sample_size)
 
         self.select_flag = False

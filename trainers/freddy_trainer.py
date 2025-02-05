fa840df8ba76e93e92e3a0cd2385ade2fce8e7af
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-05 16:12:10 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -324,7 +324,7 @@ class FreddyTrainer(SubsetTrainer):
         grad2 = grad2.pop()
         grad2 = grad2.weight.grad.data.norm(2).item()
         error = abs(grad2 - grad1) / self.importance_score[self.subset].mean()
-        print(f"relative error [{error:.4f}/{self.cur_error:.4f}]")
+        print(f"relative error [{error / self.cur_error:.4f}]")
         self.cur_error = error
         if self.cur_error < error > 10e-2:
             self._select_subset(epoch, len(self.train_loader) * epoch)

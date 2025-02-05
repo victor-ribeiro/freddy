5b55af6819b6a83260bdc5535fe27fcdd89661ff
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-05 11:21:47 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -340,8 +340,6 @@ class FreddyTrainer(SubsetTrainer):
 
         importance = (loss_t2 - loss_t1) / self.train_acc.avg
         # flag
-        print(len(self.subset))
-        exit()
         if importance[self.subset].mean() < 10e-3:
             self.select_flag = True
         # [*self.model.to(self.args.device).modules()]

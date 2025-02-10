ef758d86751aad67204a28f42d18bf3d955c7488
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-09 22:01:07 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -355,7 +355,7 @@ class FreddyTrainer(SubsetTrainer):
             loss_t2 = self.train_criterion(pred, target).cpu().detach().numpy()
 
         # importance = np.abs(loss_t2 - loss_t1)
-        importance = loss_t2 - loss_t1
+        importance = (loss_t2 - loss_t1) / loss_t2
         # self.importance_score[data_idx] -= importance
         self.importance_score[data_idx] += importance
 

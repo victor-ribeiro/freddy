9357c10c342397dabd54bf9c32cd01fd1e1153f4
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-14 10:35:39 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -326,11 +326,11 @@ class FreddyTrainer(SubsetTrainer):
         # print(f"relative error [{error}]")
         print(f"relative error [{self.cur_error}]")
 
+        self.cur_error = (self.importance_score[self.subset] - importance).mean()
+        self.cur_error = abs(self.cur_error)
         # if abs(self.cur_error - error) < 10e-3:
         if self.cur_error < 10e-3:
             self._select_subset(epoch, len(self.train_loader) * epoch)
-            self.cur_error = (self.importance_score[self.subset] - importance).mean()
-            self.cur_error = abs(self.cur_error)
         if self.hist:
             self.hist[-1]["reaL_error"] = self.cur_error
 

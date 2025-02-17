7110a649f18bec34f1d6551e06ec9873b38e7c20
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-17 11:40:43 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -225,7 +225,7 @@ class FreddyTrainer(SubsetTrainer):
 
         with torch.no_grad():
             pred = map(lambda x: self.model.cpu()(x[0]), self.train_loader)
-            pred = map(np.argmax, pred)
+            pred = map(torch.argmax, pred)
             pred = map(lambda x: one_hot_coding(x, classes=10), pred)
             tgt = map(lambda x: one_hot_coding(x[1], classes=10), self.train_loader)
             print(next(pred))

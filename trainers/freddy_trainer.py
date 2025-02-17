37ac0e3a88d56f46624686ace97af69a4fab67de
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-17 11:12:26 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -222,8 +222,8 @@ class FreddyTrainer(SubsetTrainer):
             enumerate(self.train_loader), total=len(self.train_loader), file=sys.stdout
         )
         with torch.no_grad():
-            print(self.train_dataset.dataset)
-            print(self.train_dataset.target)
+            print(self.train_dataset.dataset[:3])
+            print(self.train_dataset.dataset.targets)
             exit()
         for batch_idx, (data, target, data_idx) in pbar:
 

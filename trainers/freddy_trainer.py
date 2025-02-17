ebcfcce6ad7c8bc6f698bf8eec964668931310b6
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-17 14:35:18 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -192,8 +192,8 @@ class FreddyTrainer(SubsetTrainer):
             # feat = map(lambda x: x[1] - x[0], feat)
             feat = map(lambda x: x[1], feat)
             feat = np.vstack([*feat])
-            self.args.alpha += self.cur_error * self.args.alpha
-            self.args.beta += self.cur_error * self.args.beta
+            self.args.alpha *= self.cur_error * self.args.alpha
+            self.args.beta *= self.cur_error * self.args.beta
             # feat = feat - self.importance_score.reshape(-1, 1)
             # feat = feat * (self.importance_score.reshape(-1, 1) + self.cur_error)
 
@@ -287,7 +287,9 @@ class FreddyTrainer(SubsetTrainer):
                 importance = reduce(lambda a, b: a + b, importance)
                 # importance = np.hstack([*importance])
                 # self.importance_score[self.subset] += importance
-                self.importance_score[self.subset] -= importance
+                self.importance_score[self.subset] -= (
+                    importance / self.importance_score[self.subset].sum()
+                )
         if self.hist:
             self.hist[-1]["avg_importance"] = self.importance_score[self.subset].mean()
             self.hist[-1]["reaL_error"] = self.cur_error

cbf92a41b2dc34c5b217af8f1c0711d55079daba
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-17 10:49:10 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -51,6 +51,97 @@ def similarity(dataset, metric="euclidean", batch_size=1):
     return d.max(axis=1) - d
 
 
+def base_inc(alpha=1):
+    alpha = abs(alpha)
+    return math.log(1 + alpha)
+
+
+def utility_score(e, sset, /, acc=0, alpha=0.1, beta=1.1):
+    gamma = (alpha + beta) / 2
+    norm = 1 / base_inc(alpha)
+    argmax = np.maximum(e, sset)
+    f_norm = alpha / (sset.sum() + acc + 1)
+    util = norm * math.log(1 + (argmax.sum() + acc) * f_norm)
+    return util + (math.log(1 + (sset.sum() + acc)) * beta)
+
+
+class Queue(list):
+    def __init__(self, *iterable):
+        super().__init__(*iterable)
+        heapq._heapify_max(self)
+
+    def append(self, item: "Any"):
+        super().append(item)
+        heapq._siftdown_max(self, 0, len(self) - 1)
+
+    def pop(self, index=-1):
+        el = super().pop(index)
+        if not self:
+            return el
+        val, self[0] = self[0], el
+        heapq._siftup_max(self, 0)
+        return val
+
+    @property
+    def head(self):
+        return self.pop()
+
+    def push(self, idx, score):
+        item = (idx, score)
+        self.append(item)
+
+
+@_register
+def freddy(
+    dataset,
+    base_inc=base_inc,
+    alpha=0.15,
+    metric="similarity",
+    K=1,
+    batch_size=128,
+    beta=0.75,
+    return_vals=False,
+):
+    # basic config
+    base_inc = base_inc(alpha)
+    idx = np.arange(len(dataset))
+    # idx = np.random.permutation(idx)
+    q = Queue()
+    sset = []
+    vals = []
+    argmax = 0
+    inc = 0
+    for ds, V in zip(
+        batched(dataset, batch_size),
+        batched(idx, batch_size),
+    ):
+        D = METRICS[metric](ds, batch_size=batch_size)
+        size = len(D)
+        localmax = np.amax(D, axis=1)
+        argmax += localmax.sum()
+        _ = [q.push(base_inc, i) for i in zip(V, range(size))]
+        while q and len(sset) < K:
+            score, idx_s = q.head
+            s = D[:, idx_s[1]]
+            score_s = utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
+            inc = score_s - score
+            if (inc < 0) or (not q):
+                break
+            score_t, idx_t = q.head
+            if inc > score_t:
+                score = utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
+                localmax = np.maximum(localmax, s)
+                sset.append(idx_s[0])
+                vals.append(score)
+            else:
+                q.push(inc, idx_s)
+            q.push(score_t, idx_t)
+    np.random.shuffle(sset)
+    if return_vals:
+        return np.array(vals), sset
+    return np.array(sset)
+
+
 class FreddyTrainer(SubsetTrainer):
     def __init__(
         self,
@@ -100,22 +191,13 @@ class FreddyTrainer(SubsetTrainer):
             # feat = feat - self.importance_score.reshape(-1, 1)
             # feat = feat * (self.importance_score.reshape(-1, 1) + self.cur_error)
 
-        if self.grad_freddy:
-            sset = grad_freddy(
-                feat,
-                K=self.sample_size,
-                metric=self.args.freddy_similarity,
-                alpha=self.args.alpha,
-                beta=self.args.beta,
-            )
-        else:
-            sset = freddy(
-                feat,
-                K=self.sample_size,
-                metric=self.args.freddy_similarity,
-                alpha=self.args.alpha,
-                beta=self.args.beta,
-            )
+        sset = freddy(
+            feat,
+            K=self.sample_size,
+            metric=self.args.freddy_similarity,
+            alpha=self.args.alpha,
+            beta=self.args.beta,
+        )
         self.subset = sset
         self.selected[sset] += 1
         self.train_checkpoint["selected"] = self.selected

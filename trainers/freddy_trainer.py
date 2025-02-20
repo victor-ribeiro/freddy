5f3530cd1b53ce9ccfc466bf9a2dd01d6afc51e8
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-20 11:34:59 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -240,8 +240,6 @@ class FreddyTrainer(SubsetTrainer):
 
             feat = map(lambda x: ((x[1] - x[0]) ** 2), feat)
             feat = np.vstack([*feat])
-            print(feat.shape)
-            exit()
             feat = np.vstack([*feat]) - (
                 self.cur_error * self.importance_score.reshape(-1, 1)
             )

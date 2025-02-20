22a165933e066c8d8190d931453dcd538e429ffc
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-20 14:56:46 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -221,31 +221,29 @@ class FreddyTrainer(SubsetTrainer):
         if self.epoch_selection:
             print(f"RESELECTING: {self.epoch_selection[-1]}")
         # dataset = self.train_dataset.dataset
-        # dataset = DataLoader(
-        #     dataset,
-        #     batch_size=self.args.batch_size,
-        #     shuffle=True,
-        #     num_workers=self.args.num_workers,
-        # )
+        dataset = DataLoader(
+            dataset,
+            batch_size=self.args.batch_size,
+            shuffle=True,
+            num_workers=self.args.num_workers,
+        )
 
-        # # with torch.no_grad():
+        with torch.no_grad():
 
-        # feat = map(
-        #     lambda x: (
-        #         self.model.cpu()(x[0]).detach().numpy(),
-        #         one_hot_coding(x[1].cpu().detach().numpy(), self.args.num_classes),
-        #     ),
-        #     dataset,
-        # )
+            feat = map(
+                lambda x: (
+                    self.model.softmax(dim=1).cpu()(x[0]).detach().numpy(),
+                    one_hot_coding(x[1].cpu().detach().numpy(), self.args.num_classes),
+                ),
+                dataset,
+            )
 
-        # feat = map(lambda x: ((x[1] - x[0]) ** 2), feat)
-        feat = self.train_softmax[self.subset]
-        print(feat.shape)
-        exit()
-        # feat = np.vstack([*feat])
-        # feat = np.vstack([*feat]) - (
-        #     self.cur_error * self.importance_score.reshape(-1, 1)
-        # )
+            feat = map(lambda x: ((x[1] - x[0]) ** 2), feat)
+
+            feat = np.vstack([*feat])
+            # feat = np.vstack([*feat]) - (
+            #     self.cur_error * self.importance_score.reshape(-1, 1)
+            # )
         feat = np.vstack([*feat])
 
         if self.grad_freddy:

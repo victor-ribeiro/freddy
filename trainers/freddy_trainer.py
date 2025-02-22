fead824aa82d91eb722f5954c7b8a1fb484a20f1
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-22 10:03:20 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -134,7 +134,6 @@ def freddy(
                 break
             score_t, idx_t = q.head
             print(importance[idx_s[1]])
-            exit()
             if inc > score_t:
                 score = (
                     utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)

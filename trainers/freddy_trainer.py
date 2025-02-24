0850c39d8e38460e869cf36f0e724dc47771e0bd
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-24 13:51:23 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -286,7 +286,8 @@ class FreddyTrainer(SubsetTrainer):
     def _forward_and_backward(self, data, target, data_idx):
         self.model.eval()
         with torch.no_grad():
-            pred = self.model.cpu()(data).softmax(dim=1)
+            pred = self.model.to(self.args.device)(data).softmax(dim=1)
+            # self.model.cpu()(x[0]).softmax(dim=1).detach().numpy(),
 
             # pred = torch.argmax(pred, dim=1).float()
             # pred = torch.nn.functional.one_hot(
@@ -296,7 +297,7 @@ class FreddyTrainer(SubsetTrainer):
 
         loss, train_acc = super()._forward_and_backward(data, target, data_idx)
         with torch.no_grad():
-            pred = self.model.cpu()(data).softmax(dim=1)
+            pred = self.model.to(self.args.device)(data).softmax(dim=1)
             loss_t2 = self.train_criterion(pred, target).cpu().detach().numpy()
 
         # importance = np.abs(loss_t2 - loss_t1)

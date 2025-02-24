2b9212f6dc23b4b5d31a76a3c4c100123e1a4a54
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-24 11:25:37 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -121,13 +121,13 @@ def freddy(
         size = len(D)
         localmax = np.amax(D, axis=1)
         argmax += localmax.sum()
-        _ = [q.push(base_inc / importance[i[0]], i) for i in zip(V, range(size))]
+        _ = [q.push(base_inc * importance[i[0]], i) for i in zip(V, range(size))]
         while q and len(sset) < K:
             score, idx_s = q.head
             s = D[:, idx_s[1]]
             score_s = (
                 utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
-            ) / importance[idx_s[1]]
+            ) * importance[idx_s[1]]
             inc = score_s - score
             if (inc < 0) or (not q):
                 break
@@ -135,7 +135,7 @@ def freddy(
             if inc > score_t:
                 score = (
                     utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
-                ) / importance[idx_s[1]]
+                ) * importance[idx_s[1]]
 
                 localmax = np.maximum(localmax, s)
                 sset.append(idx_s[0])

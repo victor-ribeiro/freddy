65d72bbaeebf20a4a7d2e2d979c2d6bff3b2d80d
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-25 14:52:49 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -223,11 +223,11 @@ class FreddyTrainer(SubsetTrainer):
             data_time = time.time() - data_start
             self.batch_data_time.update(data_time)
 
-            self.optimizer.zero_grad()
             g = {
                 name: param.grad.clone()
                 for name, param in self.model.cpu().named_parameters
             }
+            self.optimizer.zero_grad()
             print(g)
             exit()
             # train model with the current batch and record forward and backward time

44bef51b29e862465174a0b2274545105d6c3153
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-25 19:07:55 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -212,7 +212,7 @@ class FreddyTrainer(SubsetTrainer):
     def _train_epoch(self, epoch):
         self.model.train()
         self._reset_metrics()
-        if self.cur_error < 103 - 3:
+        if self.cur_error < 10e-3:
             self._select_subset(epoch, len(self.train_loader) * epoch)
             self._update_train_loader_and_weights()
 

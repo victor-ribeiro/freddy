815ee592b278c89a075096d8db1fa85000da1739
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-25 17:19:51 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -282,6 +282,8 @@ class FreddyTrainer(SubsetTrainer):
         grad = torch.autograd.grad(
             loss, self.model.parameters(), retain_graph=True, create_graph=True
         )
+        print(len(grad))
+        exit()
         grad = reduce(lambda x, y: torch.sum(x[0], y[0], dim=1), grad)
         print(grad)
         print(type(grad))

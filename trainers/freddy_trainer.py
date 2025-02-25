3aabb0510276ce928dd016348df567b212878d50
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-25 18:40:01 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -186,7 +186,8 @@ class FreddyTrainer(SubsetTrainer):
         with torch.no_grad():
             delta = map(self._update_delta, dataset)
             # delta = map(lambda x: x[1] - x[0], delta)
-            self.delta += np.vstack([*delta]) * lr
+            self.delta = np.vstack([*delta])
+            self._relevance_score = np.linalg.norm(self.delta, axis=1)
 
         sset = freddy(
             self.delta,

989b897b6a122fec34888d78796b0ef52262ff1e
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 16:45:41 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -317,7 +317,7 @@ class FreddyTrainer(SubsetTrainer):
             data = data.to(self.args.device)
             loss = self.model(data).softmax(dim=1)
             delta_loss = self.model(data + e).softmax(dim=1)
-        return (loss - delta_loss).norm(2)
+        return (loss - delta_loss).norm(2, dim=1)
 
     # def train(self):
     #     self._select_subset(0, 0)

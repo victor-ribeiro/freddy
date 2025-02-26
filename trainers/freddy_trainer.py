6c255bb8dfd6e71a4edfed26e06c297e7c42cffd
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-25 22:38:57 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -177,18 +177,20 @@ class FreddyTrainer(SubsetTrainer):
         self.epoch_selection.append(epoch)
         lr = self.lr_scheduler.get_last_lr()[0]
         # if not epoch or self.cur_error > 0.05:
-
-        dataset = self.train_dataset.dataset
-        dataset = DataLoader(
-            dataset,
-            batch_size=self.args.batch_size,
-            shuffle=True,
-            num_workers=self.args.num_workers,
-        )
-        with torch.no_grad():
-            delta = map(self._update_delta, dataset)
-            # delta = map(lambda x: x[1] - x[0], delta)
-            self.delta += np.vstack([*delta]) / self.cur_error
+        if self.cur_error > 1:
+            dataset = self.train_dataset.dataset
+            dataset = DataLoader(
+                dataset,
+                batch_size=self.args.batch_size,
+                shuffle=True,
+                num_workers=self.args.num_workers,
+            )
+            with torch.no_grad():
+                delta = map(self._update_delta, dataset)
+                # delta = map(lambda x: x[1] - x[0], delta)
+                self.delta = np.vstack([*delta])
+        else:
+            self.delta += self.cur_error
 
         # self._relevance_score = np.linalg.norm(self.delta, axis=1) ** -1
 

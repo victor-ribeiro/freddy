342942a59c6faca93ec6e43d3c2638e39ed05ef7
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 15:10:12 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -220,7 +220,9 @@ class FreddyTrainer(SubsetTrainer):
             ]
             lr = self.lr_scheduler.get_last_lr()[0]
             rel_error = np.mean(rel_error)
-            self.cur_error = abs(rel_error) * lr
+            self.cur_error = abs(rel_error)
+        else:
+            self.cur_error -= self._relevance_score[self.subset].mean() * lr
 
         if not epoch:
             self._select_subset(epoch, len(self.train_loader) * epoch)

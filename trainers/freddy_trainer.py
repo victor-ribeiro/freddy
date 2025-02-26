cb2c890bb45c54ed6bd2b78721c714097198682a
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 14:26:11 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -230,12 +230,12 @@ class FreddyTrainer(SubsetTrainer):
             # if epoch % 10 == 0:
             self._select_subset(epoch, len(self.train_loader) * epoch)
             self._update_train_loader_and_weights()
-            rel_error = [
-                self._error_func(data.to(self.args.device), target.to(self.args.device))
-                for data, target, _ in self.train_loader
-            ]
-            rel_error = np.mean(rel_error)
-            # self.cur_error = abs(self.cur_error)
+        rel_error = [
+            self._error_func(data.to(self.args.device), target.to(self.args.device))
+            for data, target, _ in self.train_loader
+        ]
+        rel_error = np.mean(rel_error)
+        # self.cur_error = abs(self.cur_error)
         data_start = time.time()
         pbar = tqdm(
             enumerate(self.train_loader), total=len(self.train_loader), file=sys.stdout

5c9d7cf2e06a0180cf119c850aa6a75daa06ba91
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 14:34:07 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -288,7 +288,7 @@ class FreddyTrainer(SubsetTrainer):
         from functools import reduce
 
         lr = self.lr_scheduler.get_last_lr()[0]
-        data = data.to(self.args.device)
+        data = data.cpu()
         pred = self.model(data)
         loss = self.val_criterion(pred, target)
         model = self.model

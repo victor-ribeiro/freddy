55375ca743901d87c2567f2f704b050872248d8e
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-25 21:06:05 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -280,12 +280,15 @@ class FreddyTrainer(SubsetTrainer):
         )
         g = reduce(lambda x, y: x[0] + y[0], grad[0])
         g = g.sum().norm(2).item() * lr
-        hess = [
-            torch.autograd.grad(
-                g, self.model.parameters(), retain_graph=True, grad_outputs=g
-            )[0][0]
-            for g in grad
-        ]
+        w = [*self.model.parameters()]
+        w = w[-2]
+        # hess = [
+        #     torch.autograd.grad(
+        #         g, self.model.parameters(), retain_graph=True, grad_outputs=g
+        #     )[0][0]
+        #     for g in grad
+        # ]
+        hess = torch.autograd.grad(grad, w, retain_graph=True)
         gg = reduce(lambda x, y: x + y, hess)
         gg = gg.norm(2).item() * lr
         f = self._relevance_score[self.subset].mean()

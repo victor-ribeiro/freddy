d5a1c8ffff4b97553b1c3d88608e39e7732a568d
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 17:23:08 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -257,7 +257,7 @@ class FreddyTrainer(SubsetTrainer):
 
     def calc_embbeding(self, train_data, ord=1):
         data, target = train_data
-        data, target = data.cpu(), target.cpu()
+        # data, target = data.cpu(), target.cpu()
         # data, target = data.to(self.args.device), target.to(self.args.device)
         pred = self.model(data)
         loss = self.val_criterion(pred, target)
@@ -279,6 +279,7 @@ class FreddyTrainer(SubsetTrainer):
 
     def _update_delta(self, train_data):
         data, _ = train_data
+        data = data.cpu()
         self.model.eval()
         e = torch.normal(0, 1, size=data.shape).to(self.args.device)
         lr = self.lr_scheduler.get_last_lr()[0]

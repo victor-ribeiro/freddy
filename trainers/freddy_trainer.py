ff327acfaa95c69bdfb6ae341a24695182f5c4c7
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 15:01:14 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -295,8 +295,8 @@ class FreddyTrainer(SubsetTrainer):
         lr = self.lr_scheduler.get_last_lr()[0]
         with torch.no_grad():
             data = data.to(self.args.device)
-            loss = self.model(data).sofmax(dim=1)
-            delta_loss = self.model(data + e).sofmax(dim=1)
+            loss = self.model(data).softmax(dim=1)
+            delta_loss = self.model(data + e).softmax(dim=1)
         return (loss - delta_loss).detach().cpu().numpy()
 
     # def train(self):

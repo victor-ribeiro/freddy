94cd50f36f7d8413bae0faeacff767d55662a701
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 21:52:21 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -119,7 +119,7 @@ def freddy(
     ):
         v = list(V)
         ds = np.array(ds)
-        ds = relevance[v].T @ (relevance[v] @ ds).T
+        ds = relevance[v].T @ (ds @ relevance[v]).T
         print(ds)
         exit()
         D = METRICS[metric](ds, batch_size=batch_size)

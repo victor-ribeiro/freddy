1577f1de4cde104622959ad460b60bff5eca070a
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-27 12:32:00 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -120,7 +120,7 @@ def freddy(
         size = len(ds)
         v = list(V)
         D = METRICS[metric](ds, batch_size=batch_size)
-        D = (relevance[v].reshape(-1, 1) @ relevance[v].reshape(1, -1)) @ D
+        # D = (relevance[v].reshape(-1, 1) @ relevance[v].reshape(1, -1)) @ D
         # print(D)
         # exit()
         localmax = np.amax(D, axis=1)
@@ -129,7 +129,9 @@ def freddy(
         _ = [q.push(base_inc, i) for i in zip(V, range(size))]
         while q and len(sset) < K:
             score, idx_s = q.head
-            s = D[:, idx_s[1]]
+            s = D[:, idx_s[1]] @ (relevance[v] @ relevance[v])
+            print(s)
+            exit()
             score_s = utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
             inc = score_s - score
             if (inc < 0) or (not q):

564f11326986f56758e261d52a274637d6d95609
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-27 17:08:23 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -287,8 +287,10 @@ class FreddyTrainer(SubsetTrainer):
         f = self._update_delta((data, target))
         grad = torch.autograd.grad(loss, w, retain_graph=True, create_graph=True)[0]
 
-        g = torch.dot(f, grad.T)
+        # g = torch.inner(f, grad.T)
         # g = torch.inner(g, grad)
+        g = torch.dot(f, grad.T)
+        g = torch.dot(g, grad)
 
         hess = torch.autograd.grad(grad, w, retain_graph=True, grad_outputs=grad)[0]
         gg = torch.dot(f, hess.T)

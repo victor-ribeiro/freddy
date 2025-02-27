5b26dd62f2f6c76b360d59534271fefcb4e6dc1b
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-26 23:16:36 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -168,7 +168,7 @@ class FreddyTrainer(SubsetTrainer):
         n = len(train_dataset)
         self.epoch_selection = []
         self.delta = np.random.normal(0, 1, (n, self.args.num_classes))
-        self._relevance_score = np.ones(n)
+        self._relevance_score = np.random.normal(0, 1, n)
         self.select_flag = True
         self.cur_error = 0
 

9102c4cbce7c6795be3bcc7098b4eac30e8fc736
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-28 13:23:52 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -163,7 +163,7 @@ def shannon_entropy(vector, epsilon=1e-10):
     p = abs_vector / total
     p = p[p > 0]  # Remove zeros to avoid log(0)
     # p += 1  # Remove zeros to avoid log(0)
-    return -(p * np.log2(p))
+    return 1 - (p * np.log2(p))
 
 
 class FreddyTrainer(SubsetTrainer):

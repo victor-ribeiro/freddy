a639d14c0483b4216e6c042b02293c1aa83828d8
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-02-28 14:51:49 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -266,7 +266,7 @@ class FreddyTrainer(SubsetTrainer):
             self.hist[-1]["reaL_error"] = self.cur_error
 
         self.cur_error = abs(self.cur_error - train_loss)
-        if self.cur_error > self.train_loss.avg or not epoch:
+        if self.cur_error < self.train_loss.avg or not epoch:
             self._select_subset(epoch, len(self.train_loader) * epoch)
         # self._relevance_score[self.subset] -= (
         #     shannon_entropy(self.delta[self.subset]).mean() * lr

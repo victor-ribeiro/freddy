525c738e31bf834993c9cfcefc96dac56f4dda74
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-06 16:15:50 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -58,11 +58,12 @@ def base_inc(alpha=1):
 
 
 def utility_score(e, sset, /, acc=0, alpha=0.1, beta=1.1):
+    beta = 0
     gamma = (alpha + beta) / 2
     norm = 1 / base_inc(alpha)
     argmax = np.maximum(e, sset)
     f_norm = alpha / (sset.sum() + acc + 1)
-    print(1 + (argmax.sum() + acc) * f_norm, f_norm)
+    f_norm = min(0, f_norm)
     util = norm * math.log(1 + (argmax.sum() + acc) * f_norm)
     return util + (math.log(1 + (sset.sum() + acc)) * beta)
 

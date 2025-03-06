d215917392c24479b51e2d1837971e6ca7b65154
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-06 15:03:15 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -132,7 +132,10 @@ def freddy(
         r = r / np.linalg.norm(r)
         if r @ -v_i > 0:
             v_i = -v_i
+        # linear penalty
+        print(-r @ v_i)
         r = r @ v_i - np.max(0, -r @ v_i)
+        # exponential penalty
         print(v_i)
 
         exit()

2097672f039de94ca64603642db229148aa5e8bf
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-06 15:22:58 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -300,6 +300,7 @@ class FreddyTrainer(SubsetTrainer):
         loss = self.val_criterion(pred, target)
         w = [*self.model.modules()]
         w = (w[-1].weight,)
+        return self._update_delta((data, target))
         f = self._update_delta((data, target))
         grad = torch.autograd.grad(loss, w, retain_graph=True, create_graph=True)[0]
         g = torch.inner(f, grad.T)
@@ -318,7 +319,7 @@ class FreddyTrainer(SubsetTrainer):
             data = data.to(self.args.device)
             loss = self.model(data)
             delta_loss = self.model(data + e).softmax(dim=1)
-        return loss - delta_loss
-        # return (loss - target).cpu().detach().numpy()
+        # return loss - delta_loss
+        return (loss - target).cpu().detach().numpy()
         return loss - target
         # return loss - delta_loss

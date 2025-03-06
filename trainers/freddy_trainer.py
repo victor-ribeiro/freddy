53ad05355665ba2aec089fbe6af0ea2ada0dce65
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-06 17:10:08 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -93,7 +93,7 @@ class Queue(list):
         self.append(item)
 
 
-def linear_selector(r, v1, k, lambda_=0.1):
+def linear_selector(r, v1, k, lambda_=0.5):
     from scipy.optimize import linprog
 
     """

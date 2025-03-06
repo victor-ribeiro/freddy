ae3a888984070c41cf768adec7818e153c32483f
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-06 11:13:02 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -131,7 +131,8 @@ def freddy(
         while q and len(sset) < K:
             score, idx_s = q.head
             s = D[idx_s[1], :]
-            s -= alpha * (relevance[v].reshape(-1, 1) @ max_eigenvector)
+            # s = s @ (relevance[v].reshape(-1, 1) @ max_eigenvector)
+            s -= alpha * (relevance[v] @ max_eigenvector)
             score_s = utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
             inc = score_s - score
             if (inc < 0) or (not q):

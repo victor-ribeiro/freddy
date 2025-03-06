fae676cac11a1c599d000304109bcfb3887e4870
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-06 16:20:39 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -288,7 +288,8 @@ class FreddyTrainer(SubsetTrainer):
             self._update_train_loader_and_weights()
         # self.delta[self.subset] += self.cur_error * lr
         # self._relevance_score -= self._relevance_score * lr
-        self.cur_error = abs(self.cur_error - self._relevance_score[self.subset].mean())
+        # self.cur_error = abs(self.cur_error - self._relevance_score[self.subset].mean())
+        self.cur_error = abs(self.cur_error - train_loss)
 
     def f_embedding(self):
         dataset = self.train_dataset.dataset

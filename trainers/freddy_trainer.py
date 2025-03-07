abad052c80619889f517b3a9d671387228c15c80
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-07 15:00:18 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -240,7 +240,7 @@ def freddy(
         while q and len(sset) < K:
             score, idx_s = q.head
             # s = D[idx_s[1], :] * relevance[idx_s[0]]
-            s = D[idx_s[1], :] + penalty
+            s = D[idx_s[1], :] - penalty
             score_s = utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
             inc = score_s - score
             if (inc < 0) or (not q):

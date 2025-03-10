82f789001d0f67a1a1ff529e69a9b4d9f0740f6d
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-10 18:24:09 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -166,7 +166,7 @@ def freddy(
         D = METRICS[metric](ds, batch_size=batch_size)
         V = np.array(V)
         # r = D @ relevance[V]
-        r = shannon_entropy(D) / relevance[V]
+        r = shannon_entropy(ds) * relevance[V]
         eigenvals, eigenvectors = np.linalg.eigh(D)
         max_eigenval = np.argsort(eigenvals)[-1]
         v1 = eigenvectors[max_eigenval]

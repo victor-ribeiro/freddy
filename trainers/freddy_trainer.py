9879cc0ce7eb495ce9a2e6697827a834f3c9b4a1
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-10 10:33:46 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -161,11 +161,10 @@ def freddy(
         # D = METRICS["codist"](ds, batch_size=batch_size)
         V = np.array(V)
         # r = D @ relevance[V]
-        r = np.log(1 + D.sum(axis=1) * relevance[V])
+        s = D.sum(axis=1) * relevance[V]
+        s = np.maximum(0, s)
+        r = np.log(1 + s)
         print("r1", r)
-        r = np.maximum(0, r)
-        print("r2", r)
-        print("r2+1", r + 1)
         eigenvals, eigenvectors = np.linalg.eigh(D)
         max_eigenval = np.argsort(eigenvals)[-1]
         v1 = eigenvectors[max_eigenval]

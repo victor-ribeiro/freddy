d2a141fd1c17e82b241cf35adbcebddbf7d536b5
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-11 14:55:08 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -122,7 +122,9 @@ def freddy(
         D = METRICS[metric](ds, batch_size=batch_size)
         lambda_, v1 = np.linalg.eigh(D)
         i = np.argmax(lambda_)
-        v1 = np.maximum(0, v1[i])
+        if v1 @ relevance[V] < 0:
+            v1 = -v1
+        v1 = np.maximum(0, v1)
 
         size = len(D)
         localmax = np.amax(D, axis=1)

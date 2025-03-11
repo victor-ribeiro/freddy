2c141c58d411155d755d4b1ae811d2f486a07520
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-11 11:05:38 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -229,7 +229,7 @@ class FreddyTrainer(SubsetTrainer):
         self.f_embedding()
         sset, score = freddy(
             self.delta,
-            lambda_=self.cur_error,
+            lambda_=self.lambda_,
             batch_size=128,
             K=self.sample_size,
             metric=self.args.freddy_similarity,

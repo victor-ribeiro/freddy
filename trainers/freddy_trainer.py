3df8720b4797d7d206387a73d23663b713ba2c7a
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-12 07:49:44 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -150,7 +150,7 @@ def freddy(
                 sset.append(idx_s[0])
                 vals.append(score)
                 alpha = min(10, alpha * 1.1)
-                base += inc
+                base_inc += inc
             else:
                 alpha = max(0.5, alpha * 0.8)
                 q.push(inc, idx_s)

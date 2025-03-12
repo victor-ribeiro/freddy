4e73f6cd3d7a119b37561d7d197aab562c308af6
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-12 14:16:14 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -132,7 +132,7 @@ def freddy(
         direct = v1 + direct[:size]
 
         # g = direct.reshape(-1, 1) @ (np.eye(size) * relevance[V])
-        g = direct * (np.eye(size) * relevance[V])
+        g = direct * (np.eye(size) @ relevance[V])
 
         D += D @ g
 

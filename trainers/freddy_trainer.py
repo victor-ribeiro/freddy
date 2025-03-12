a3e6c7a3ccc6be86a046e7c6f4bc5105f10dd7cc
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-12 20:58:57 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -454,7 +454,7 @@ class FreddyTrainer(SubsetTrainer):
         hess = torch.autograd.grad(grad, w, retain_graph=True, grad_outputs=grad)[0]
         gg = torch.inner(f, hess.T)
         # gg = torch.inner(gg, hess)
-        # W = g + (gg / 2)
+        W = g + (gg / 2)
         return torch.inner(torch.inner(f, W), W.T).cpu().detach().numpy()
         # return (f + g + (gg / 2)).cpu().detach().numpy()
 

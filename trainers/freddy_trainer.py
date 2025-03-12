148903b3c2de798fbb7942c40697fba34a1351e7
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-12 14:30:56 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -388,7 +388,11 @@ class FreddyTrainer(SubsetTrainer):
         # self._relevance_score += self._relevance_score * lr
         # self.cur_error = abs(self.cur_error - train_loss)
         # print(shannon_entropy(self.delta[self.subset].mean()).shape)
-        self._relevance_score = 1 - shannon_entropy(self.delta[self.subset].mean())
+        self._relevance_score[self.subset] = shannon_entropy(
+            self.delta[self.subset].mean()
+        )
+        print(self._relevance_score)
+        exit()
 
         self.cur_error = self._relevance_score[self.subset].mean()
 

10306e54e27ea5e823d491422f06cfc286e9d711
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-12 21:21:39 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -233,7 +233,7 @@ def _n_cluster(dataset, alpha=1, max_iter=100, tol=10e-2):
 
 
 def kmeans_sampler(dataset, K, alpha=1, tol=10e-3, max_iter=500, relevance=None):
-    dataset *= relevance
+    dataset *= relevance.reshape(-1, 1)
     idx = np.where(relevance > 0)
     min_size = math.ceil(len(dataset) * 0.8)
     if len(idx) > min_size:

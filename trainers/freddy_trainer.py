f6b2a6abfdf16a0cb12049dc36fc9071e9c08f5d
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-12 22:01:43 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -232,8 +232,8 @@ def _n_cluster(dataset, alpha=1, max_iter=100, tol=10e-2):
     return ValueError("Does not converge")
 
 
-def kmeans_sampler(dataset, K, alpha=1, tol=10e-3, max_iter=500, relevance=None):
-    dataset += dataset * relevance.reshape(-1, 1)
+def kmeans_sampler(dataset, K, alpha=1.5, tol=10e-3, max_iter=500, relevance=None):
+    dataset += dataset * -relevance.reshape(-1, 1)
     idx = np.where(relevance > 0)
     min_size = math.ceil(len(dataset) * 0.8)
     if len(idx) > min_size:

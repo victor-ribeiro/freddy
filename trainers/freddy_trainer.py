8a28c51df6ec955f6419d2d72c85f6193b03055d
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-13 22:51:06 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -337,7 +337,7 @@ class FreddyTrainer(SubsetTrainer):
 
         self.targets[epoch] += target[sset].sum(axis=0)
         p = self.targets.sum(axis=0) / self.targets.sum()
-        score = feat * -(p * np.log2(1 + p)).sum()
+        score = feat * (-(p * np.log2(1 + p)).sum()) / np.log2(len(dataset))
         self._relevance_score = (1 / (score + 10e-8)).sum(axis=1)
         print(f"selected ({len(sset)}) [{epoch}]: {self.targets[epoch]}")
         self.subset = sset

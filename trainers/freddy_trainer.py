4b8f3cf0357356aa2f02dc07944f6c03c7ac9fb6
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-16 15:58:11 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -230,11 +230,15 @@ def _n_cluster(dataset, k=1, alpha=1, max_iter=100, tol=10e-2, relevance=None):
         sampler.fit(dataset)
         if val[:idx].sum() == 0:
 
-            val[idx] = np.log(1 + sampler.inertia_) - base
+            val[idx] = np.log(1 + sampler.inertia_) - base + np.log(relevance.mean())
             # val[idx] += np.exp(val[idx] - relevance.sum())
             continue
 
-        val[idx] = np.log(sampler.inertia_ / val[val > 0].mean()) - base
+        val[idx] = (
+            np.log(sampler.inertia_ / val[val > 0].mean())
+            - base
+            + np.log(relevance.mean())
+        )
         # val[idx] += np.exp(val[idx] - relevance.sum())
         alpha = np.log(k + 2)
         if abs(val[:idx].min() - val[idx]) < tol:

521204e11e5e51c70b3fa554ed1f84f3b57c8ab4
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-16 15:08:10 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -4,7 +4,7 @@ import math
 from itertools import batched
 from functools import reduce
 from sklearn.metrics import pairwise_distances
-from sklearn.cluster import BisectingKMeans, DBSCAN
+from sklearn.cluster import BisectingKMeans, Birch
 
 
 import torch
@@ -223,7 +223,7 @@ def _n_cluster(dataset, k=1, alpha=1, max_iter=100, tol=10e-2, relevance=None):
     for idx, n in enumerate(range(max_iter)):
         base = np.log(1 + alpha)
         # sampler = BisectingKMeans(n_clusters=n + 2, init="k-means++")
-        sampler = DBSCAN(n_clusters=n + 2, n_jobs=4)
+        sampler = Birch(n_clusters=n + 2, n_jobs=4)
         sampler.fit(dataset)
         if val[:idx].sum() == 0:
 

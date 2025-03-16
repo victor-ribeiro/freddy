3a423530e4a3e8582a3ba3e820012275e3726e4d
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-16 15:30:32 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -4,7 +4,11 @@ import math
 from itertools import batched
 from functools import reduce
 from sklearn.metrics import pairwise_distances
-from sklearn.cluster import BisectingKMeans, AgglomerativeClustering
+from sklearn.cluster import (
+    BisectingKMeans,
+    AgglomerativeClustering,
+    FeatureAgglomeration,
+)
 
 
 import torch
@@ -223,7 +227,7 @@ def _n_cluster(dataset, k=1, alpha=1, max_iter=100, tol=10e-2, relevance=None):
     for idx, n in enumerate(range(max_iter)):
         base = np.log(1 + alpha)
         # sampler = BisectingKMeans(n_clusters=n + 2, init="k-means++")
-        sampler = AgglomerativeClustering(n_clusters=n + 2)
+        sampler = FeatureAgglomeration(n_clusters=n + 2)
         sampler.fit(dataset)
         if val[:idx].sum() == 0:
 

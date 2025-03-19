1f1200b20f910a17744ab063fff4259acaae950e
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-19 11:36:33 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -306,7 +306,9 @@ class FreddyTrainer(SubsetTrainer):
 
             # self.model.train()
             #### fim
-        self.clusters -= self.clusters * self.grad_norm * self.lr
+        self.clusters -= (
+            self.clusters * self.grad_norm * self.lr / len(self.train_loader)
+        )
         self._val_epoch(epoch)
 
         train_loss /= len(self.train_loader)

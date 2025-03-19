418a8fd93042e480075a93806413a4f504aaa6e8
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-19 18:28:53 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -220,7 +220,7 @@ class FreddyTrainer(SubsetTrainer):
         )
         self.targets[epoch] += tgt[sset].sum(axis=0)
         p1 = self.targets[epoch].sum(axis=0) / self.targets[epoch].sum()
-        p2 = self.targets[: epoch + 1].sum(axis=0) / self.targets.sum()
+        p2 = self.targets[: epoch + 1].sum(axis=0) / self.targets.sum() + 10e-8
         score = self.train_criterion(
             torch.from_numpy(feat), torch.from_numpy(tgt)
         ).detach().numpy() * (

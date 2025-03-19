cc9b15d21ff007c80650c8386de7119706a717b2
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-19 18:46:00 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -228,8 +228,8 @@ class FreddyTrainer(SubsetTrainer):
             # * (p1 * np.log2(1 + p1)).sum()
         )
         # score = (score.mean() - score) / score.std()
-        self._relevance_score = (
-            p2 * p1 / (np.exp(score) / (np.exp(score).sum() + 10e-8))
+        self._relevance_score = (p2 * p1 / p1) / (
+            np.exp(score) / (np.exp(score).sum() + 10e-8)
         )
         print(f"score {score}")
 

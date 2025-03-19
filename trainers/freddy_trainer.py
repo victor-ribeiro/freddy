b4e91787e4e73b9bff7277ea2b83dd87cfb782f6
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-19 10:36:39 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -110,14 +110,15 @@ def _n_cluster(dataset, k=1, alpha=1, max_iter=100, tol=10e-2, relevance=None):
         inertia = sampler.inertia_ + 10e-8
         if val[:idx].sum() == 0:
             val[idx] = np.log(1 + inertia * relevance.mean()) - base
-            val[idx] += np.exp(inertia - relevance.mean())
+            # val[idx] += np.exp(inertia - relevance.mean())
             cls[idx] = n + 2
             continue
 
         val[idx] = np.log(inertia * val[val > 0].sum() * relevance.mean()) - base
-        val[idx] += np.exp(inertia - relevance.mean())
+        print(val[:idx])
+        # val[idx] += np.exp(inertia - relevance.mean())
         cls[idx] = n + 2
-        alpha += np.log(k + 2) * 0.1
+        # alpha += np.log(k + 2) * 0.1
         if abs(val[:idx].min() - val[idx]) < tol:
             # import matplotlib.pyplot as plt
 

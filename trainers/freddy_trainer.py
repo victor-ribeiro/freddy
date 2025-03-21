543da26703cd9b5895d78d5c99b79fc6311a60c1
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-03-21 20:54:17 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -255,7 +255,7 @@ class FreddyTrainer(SubsetTrainer):
         score = (
             (
                 self.train_criterion(torch.Tensor(feat[sset]), torch.Tensor(tgt[sset]))
-                * self.train_weights[sset]
+                * self.train_weights[sset.copy()]
             )
             .detach()
             .numpy()

bc6e309f546aef67ba1c904f454b1a9b3b93f4d0
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-04-01 15:32:38 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -162,14 +162,10 @@ def pmi_kmeans_sampler(
     h_pc = entropy(np.dot(dataset, clusters.T))
     h_c = entropy(clusters)
     h_p = entropy(dataset)
-    # pmi = np.log2(K) - (h_p + h_c - h_pc)
     pmi = (h_c - h_pc) / h_p
-    # pmi = (np.array(pmi) * relevance.reshape(-1, 1)).sum(axis=1)
     pmi = dist * pmi
-
-    # pmi -= np.max(pmi, axis=0) - np.min(pmi, axis=0)
-    # pmi = np.abs(pmi)
-    sset = np.argsort(pmi, kind="heapsort")[::-1]
+    # sset = np.argsort(pmi, kind="heapsort")[::-1]
+    sset = np.argsort(pmi, kind="heapsort")
     return sset[:K]
 
 

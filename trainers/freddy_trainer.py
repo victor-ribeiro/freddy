58e1e9555987faf6eecc77b972f6c2137427ff3b
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-04-10 18:56:36 -0300

    .

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -331,12 +331,12 @@ class FreddyTrainer(SubsetTrainer):
         self.targets[epoch] += tgt[sset].sum(axis=0)
         p1 = self.targets[epoch].sum(axis=0) / self.targets[epoch].sum()
         p2 = self.targets[: epoch + 1].sum(axis=0) / self.targets.sum() + 10e-8
-        score = (
-            self.train_criterion(torch.from_numpy(feat), torch.from_numpy(tgt))
-            .detach()
-            .numpy()
-            # * -(p1 * np.log2(1 + p1)).sum()
-        )
+        # score = (
+        #     self.train_criterion(torch.from_numpy(feat), torch.from_numpy(tgt))
+        #     .detach()
+        #     .numpy()
+        #     # * -(p1 * np.log2(1 + p1)).sum()
+        # )
         # score = (
         #     self.train_criterion(
         #         torch.from_numpy(feat[sset]), torch.from_numpy(tgt[sset])
@@ -358,7 +358,8 @@ class FreddyTrainer(SubsetTrainer):
 
         # # score = (score.max() - score) / (score.max() - score.min())
         # score = (score.mean() - score) / score.std()
-        self._relevance_score = score
+        self._relevance_score[sset] += 1
+        self._relevance_score *= self._relevance_score.mean()
         # self._relevance_score[sset] = score[sset]
         # self._relevance_score[sset] += score[sset] * self.lr
         print(f"selected ({len(sset)}) [{epoch}]: {self.targets[epoch].astype(int)}")

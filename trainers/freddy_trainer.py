22e414358ab7fd3ecd9534e5e51c4a25011a5fbd
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-04-18 17:20:24 -0300

    time measue

--- trainers/freddy_trainer.py
+++ trainers/freddy_trainer.py
@@ -217,7 +217,8 @@ class FreddyTrainer(SubsetTrainer):
         self.selected = np.zeros(len(train_dataset))
         #
         n = len(train_dataset)
-        self.train_frac = 1
+        # self.train_frac = 1
+        self.train_frac = self.args.train_frac
         self.min_train_frac = self.args.train_frac
         self.sample_size = int(len(self.train_dataset) * self.min_train_frac)
         self.epoch_selection = []
@@ -292,8 +293,8 @@ class FreddyTrainer(SubsetTrainer):
         # if (epoch + 1) % 19 == 0:
         # if (epoch + 1) % 20 == 0:
         if epoch % 20 == 0:
-            self.train_frac = max(self.min_train_frac, self.train_frac - 0.2)
-            self.sample_size = int(len(self.train_dataset) * self.train_frac)
+            # self.train_frac = max(self.min_train_frac, self.train_frac - 0.2)
+            # self.sample_size = int(len(self.train_dataset) * self.train_frac)
             # print(self.sample_size)
             ##############################
             selection_init = time.perf_counter()
